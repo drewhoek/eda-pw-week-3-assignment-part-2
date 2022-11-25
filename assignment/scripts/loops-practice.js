f6cf1d2d2f1906fdcbc5da2ba03546b9f95c6b67
Drew Hoek <drewh815@gmail.com>
2022-11-25 16:29:51 -0600

    Completed question 1.a. and 1.b.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -2,21 +2,27 @@ console.log('****** Loops Practice *******');
 
 
 // 1. 'for' loop
-console.log('---- 1. For loops ----');
+console.log( '---- 1. For loops ----' );
 // Example: a for loop to console.log numbers from 0 to 3
-console.log('count from 0 to 3');
+console.log( 'count from 0 to 3' );
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i = 0; i < 4; i++) {
+for ( let i = 0; i < 4; i++ ) {
   console.log(i);
 }
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 0 to 5');
+console.log( 'count from 0 to 5' );
+for ( let i = 0; i < 6; i++ ) {
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 3 to 5');
+console.log( 'count from 3 to 5' );
+for ( let i = 3; i < 6; i++ ) {
+  console.log(i); 
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
